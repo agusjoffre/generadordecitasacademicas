348ee93ab24fe8cbed1be93f3fb6ef238635cb8d
agusjoffre <aguslj16@gmail.com>
2024-11-25 03:26:53 +0000

    updated ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,8 @@ export default function Home() {
 
   if (!contextBook) {
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-14 row-start-2 items-center sm:items-start">
-          <p>no book</p>
+      <div className="w-full grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:px-14 md:px-20 lg:px-32 xl:px-48 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-14 row-start-2 items-center sm:items-start w-full xl:w-[800px]">
           <CitationForm />
         </main>
       </div>
